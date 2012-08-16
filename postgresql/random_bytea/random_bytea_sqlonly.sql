bf971dc4c1182adbf2f0defe4352b0310477a5ba
Craig Ringer <ringerc@ringerc.id.au>
2012-08-16 18:42:54 +0800

    Fix an off-by-one error

--- postgresql/random_bytea/random_bytea_sqlonly.sql
+++ postgresql/random_bytea/random_bytea_sqlonly.sql
@@ -9,7 +9,7 @@
 CREATE OR REPLACE FUNCTION random_bytea(bytea_length integer)
 RETURNS bytea AS $body$
     SELECT decode(string_agg(lpad(to_hex(width_bucket(random(), 0, 1, 256)-1),2,'0') ,''), 'hex')
-    FROM generate_series(0, $1);
+    FROM generate_series(1, $1);
 $body$
 LANGUAGE 'sql'
 VOLATILE
