b4518ac3ad82d0d8ad410b62ac76fb353d9cdcdd
Craig Ringer <ringerc@ringerc.id.au>
2012-08-16 16:55:29 +0800

    Comments on the random_bytea functions

--- postgresql/random_bytea/random_bytea_sqlonly.sql
+++ postgresql/random_bytea/random_bytea_sqlonly.sql
@@ -1,4 +1,11 @@
+--
 -- See http://dba.stackexchange.com/a/22571/7788
+--
+--
+-- This is a simple standalone SQL-only function to generate
+-- random bytea values. It's fine for generating a few hundred kb.
+--
+
 CREATE OR REPLACE FUNCTION random_bytea(bytea_length integer)
 RETURNS bytea AS $body$
     SELECT decode(string_agg(lpad(to_hex(width_bucket(random(), 0, 1, 256)-1),2,'0') ,''), 'hex')
@@ -7,3 +14,5 @@ $body$
 LANGUAGE 'sql'
 VOLATILE
 SET search_path = 'pg_catalog';
+
+COMMENT ON random_bytea(integer) IS 'Generate n random bytes of garbage, returned as bytea. Slow for large values of n.';
