84d5b90521136cfb80abbf518c145d315d8b5bf2
Craig Ringer <craig@2ndquadrant.com>
2018-04-04 15:18:20 +0800

    Twiddle fsync test values to reproduce the problem

--- testcases/fsync-error-clear/fsync_test.sh
+++ testcases/fsync-error-clear/fsync_test.sh
@@ -13,33 +13,59 @@ do
 done
 
 # make 100mb dev and loopback it
-dd if=/dev/zero of=/tmp/errblk bs=512 count=204800
+# KB and MB are blocks-to-kb
+
+# Then map a 5k bad-block range near the middle with device-mapper. Some twiddling
+# can be needed to put it somewhere xfs doesn't try to write something important to.
+#
+# producing a fake device with the first 50mb, an error 5k, and the rest of the
+# 50mb minus the error range.
+BLOCKS_KB=2
+BLOCKS_MB=$((1024*2))
+# Total looback dev size to allocate
+TOTSZ=$((100 * $BLOCKS_MB))
+# Amount of loopback device to map before bad range. Move the end of this
+# around to make sure the error region doesn't make xfs creation fail.
+STARTSZ=$(( 58 * $BLOCKS_MB ))
+# Amount of loopback device to replace with error region
+ERRSZ=$(( 5*$BLOCKS_KB ))
+# ... and rest is mapped after
+
+printf "Creating %zu byte block dev with %zu size hole at %zu\n" $(( $TOTSZ * 512 )) $(( $ERRSZ * 512 )) $(( $STARTSZ * 512 ))
+
+dd if=/dev/zero of=/tmp/errblk bs=512 count=$TOTSZ
 
 LOOPDEV=$(losetup --show -f /tmp/errblk)
 
 trap "{ losetup -d ${LOOPDEV}; }" EXIT
 
-ls -l /dev
-ls -l /dev/mapper
-
-# map a 5k hole in the middle with device-mapper
-# producing a fake device with the first 50mb, an error 5k, and the last 50mb
 #
 # Units below are 512b blocks
 #
-dmsetup create errdev1 --addnodeoncreate <<__END__
-0 102400 linear ${LOOPDEV} 0
-102400 10 error
-102410 102400 linear ${LOOPDEV} 102400
+# Some twiddling can be needed
+#
+ 
+cat > dm_table_errdev1 <<__END__
+0				${STARTSZ}				linear ${LOOPDEV} 0
+${STARTSZ}			${ERRSZ}				error
+$(( $STARTSZ + $ERRSZ ))	$(( $TOTSZ - $STARTSZ - $ERRSZ))	linear ${LOOPDEV} $(( $STARTSZ + $ERRSZ ))
 __END__
 # (credit http://serverfault.com/q/498900/102814 for approach).
+
+echo "errdev1 table is:"
+echo "----"
+cat dm_table_errdev1
+echo "----"
+
+dmsetup create errdev1 < dm_table_errdev1
 
 trap "{ dmsetup remove errdev1; losetup -d ${LOOPDEV}; }" EXIT
 
 dmsetup mknodes errdev1
 
-# make a FS on it and mount it
 sudo mkfs.xfs /dev/mapper/errdev1
+
+# make a FS on it and mount it
 mkdir -p /mnt/tmp
 mount /dev/mapper/errdev1 /mnt/tmp
 
