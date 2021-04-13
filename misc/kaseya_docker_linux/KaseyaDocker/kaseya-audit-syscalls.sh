46109ded54e34f1c4ed2904af1651b963c06e658
Craig Ringer <craig.ringer@2ndquadrant.com>
2021-04-13 20:56:21 +0800

    wip kaseya sandbox syscalls

--- misc/kaseya_docker_linux/KaseyaDocker/kaseya-audit-syscalls.sh
+++ misc/kaseya_docker_linux/KaseyaDocker/kaseya-audit-syscalls.sh
@@ -11,6 +11,7 @@ if [ -e $setupexe ]; then
     sudo auditctl -A always,exit -F exe=$setupexe -F arch=b64 -S all -k kcs
     sudo auditctl -A never,exit -F exe=$setupexe -F dir=/opt/Kaseya -k kcs
     sudo auditctl -A always,exit -F exe=$setupexe -F dir=/ -k kcs
+    outfile=kaseya-setup
 fi
 agent=/opt/Kaseya/${GUID}/AgentMon
 if [ -e $agent ]; then
@@ -21,6 +22,7 @@ if [ -e $agent ]; then
     sudo auditctl -A always,exit -F exe=$agent -F arch=b64 -S all -k kcs
     sudo auditctl -A never,exit -F exe=$agent -F dir=/opt/Kaseya -k kcs
     sudo auditctl -A always,exit -F exe=$agent -F dir=/ -k kcs
+    outfile=kaseya
 fi
 sudo systemctl start kaseya.service
 #sleep 60
@@ -29,5 +31,5 @@ sudo systemctl stop kaseya.service
 sudo auditctl -D -k kcs
 sudo auditctl -a never,task
 
-sudo ausearch --format raw -ts recent -k kcs | aureport --syscall -i --summary > kaseya-syscalls.report || true
-sudo ausearch --format raw -ts recent -k kcs | aureport --file -i --summary > kaseya-files.report || true
+sudo ausearch --format raw -ts recent -k kcs | aureport --syscall -i --summary > $outfile-syscalls.report || true
+sudo ausearch --format raw -ts recent -k kcs | aureport --file -i --summary > $outfile-files.report || true
--- misc/kaseya_docker_linux/KaseyaDocker/kcs_make_systemd_sandbox_service.sh
+++ misc/kaseya_docker_linux/KaseyaDocker/kcs_make_systemd_sandbox_service.sh
@@ -2,6 +2,8 @@
 
 set -e -u
 
+set -x
+
 rm -rf kcs
 mkdir kcs
 
@@ -108,29 +110,81 @@ sudo mkdir -p /opt/Kaseya
 sudo cp kcs/KcsSetup /opt/Kaseya
 sudo chmod +x /opt/Kaseya/KcsSetup
 
+# This systemd service definition sandboxes the Kaseya installer and the
+# runtime agent into a confined runtime environment with minimum privileges.
+#
+# While it runs as uid 0, it has none of the special privileges of root and all
+# means of privilege elevation are disabled. Unnecessary system calls are
+# filtered so they'll return -EPERM if attempted. Most of the filesystem is
+# hidden from the agent, with select parts exposed read-only. It only has
+# read/write within its own /opt/Kaseya directory. Loading of kernel modules
+# and modification of sysfs tunables or procfs sysctls is explicitly blocked.
+# Access to hardware device nodes, any direct hardware I/O, etc is blocked.
+#
+# To understand this you'll want to see manpages:
+#
+#   systemd.unit(5)
+#   systemd.service(5)
+#   systemd.exec(5) heading "SANDBOXING"
+#   systemd.resource-control(5)
+#   capabilities(7)
+#   cgroups(7)
+#   namespaces(7)
+#   https://www.kernel.org/doc/html/latest/userspace-api/no_new_privs.html
+#
 cat > kaseya.service <<__END__
 [Service]
 ExecStartPre=-/opt/Kaseya/KcsSetup $(sed 's~/~-~g' < kcs/KaSetup_Args) -i -x -p /opt/Kaseya
 ExecStart=/opt/Kaseya/${guid}/bin/AgentMon
+# see systemd.exec(5), systemd.resource-control(5)
+RestrictNamespaces=true
+RestrictSUIDSGID=true
+SecureBits=noroot noroot-locked
+NoNewPrivileges=yes
 AmbientCapabilities=
 CapabilityBoundingSet=
-ProtectSystem=full
-ProtectHome=tmpfs
-ReadWritePaths=/opt/Kaseya
-SystemCallFilter=~@mount
-TemporaryFileSystem=/etc/profile.d
-PrivateTmp=true
-PrivateUsers=true
 ProtectHostname=true
 ProtectClock=true
 ProtectKernelTunables=true
 ProtectKernelModules=true
 ProtectControlGroups=true
-RestrictNamespaces=true
-RestrictSUIDSGID=true
-NoNewPrivileges=yes
+ProtectHome=tmpfs
+DevicePolicy=closed
+DeviceAllow=/dev/log
+#ProtectSystem=strict
+ProtectSystem=full
+#ReadWritePaths=/opt/Kaseya
+#ReadWritePaths=/run
+#ReadWritePaths=/var/run
+#ReadWritePaths=/tmp
+# Required for PrivateTmp
+#ReadWritePaths=/var/tmp
+#ReadOnlyPaths=/etc
+#ReadOnlyPaths=/dev
+#ReadOnlyPaths=/proc
+#ReadOnlyPaths=/sys
+#ReadOnlyPaths=/lib
+#ReadOnlyPaths=/bin
+#ReadOnlyPaths=/sbin
+#ReadOnlyPaths=/usr/lib
+#ReadOnlyPaths=/usr/bin
+#ReadOnlyPaths=/usr/sbin
+#TemporaryFileSystem=/etc/profile.d
+PrivateTmp=true
+PrivateUsers=true
+# See systemd-analyse syscall-filter, and systemd.exec(5)
+#SystemCallFilter=@default @process @basic-io @chown @file-system @network-io @timer
+#SystemCallFilter=~@mount @module @privileged @reboot @debug @keyring @setuid
+#SystemCallFilter=@system-service
+SystemCallFilter=~@mount
 __END__
 
 sudo cp kaseya.service /etc/systemd/system/
 sudo systemctl --system daemon-reload
 sudo systemctl start kaseya.service
+
+sleep 5
+
+systemctl status kaseya.service
+#systemd-cgls -u kaseya.service
+#journalctl -u kaseya.service
